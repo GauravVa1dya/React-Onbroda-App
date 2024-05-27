ef5cec3852590f8dc561ac3811dbed4c357ab097
Gaurav Vaidya <gauravprof2998@gmail.com>
2024-05-27 13:37:23 +0530

    first commit

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,18 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDFsPjol8trnK8ZhxUzhWlY1XdCGgHiTWs",
-  authDomain: "onbroda.firebaseapp.com",
-  projectId: "onbroda",
-  storageBucket: "onbroda.appspot.com",
-  messagingSenderId: "67553906370",
-  appId: "1:67553906370:web:513719984456a1d9f3b55b",
-  measurementId: "G-1752QSG86E"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export default app;
-
-
